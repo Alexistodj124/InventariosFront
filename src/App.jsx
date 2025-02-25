1f514f8af41dbe2e7453546a0ba1e9da6b875ed9
Alexistodj124 <alexistodj@gmail.com>
2025-02-25 11:28:31 -0600

    Responsive top bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import LoginPage from './pages/login';
+import MenuPage from './pages/menu';
 
 function App() {
-  return <LoginPage />;
+  // return <LoginPage />;
+  return <MenuPage />;
 }
 
 export default App;
---
+++ src/pages/menu/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopBar from '../global/topbar';
+import { Box } from '@mui/material';
+
+const Menu = () => {
+    return (
+        <Box sx={{ width: '100%' }}>
+            <TopBar />
+        </Box>
+    );
+};
+
+export default Menu;
